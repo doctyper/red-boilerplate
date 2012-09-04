8950ce9592b6787429a18457c6baef0256d368bb
Richard Herrera <rich@doctyper.com>
2012-09-04 15:06:43 -0700

    Automated external library retrieval.

---
+++ libs.js
@@ -0,0 +1,29 @@
+module.exports = {
+	"libs" : [{
+		"name" : "Handlebars",
+		"filename" : "handlebars.js",
+		"src" : "https://raw.github.com/SlexAxton/require-handlebars-plugin/master/Handlebars.js",
+		"pluginsPath" : "handlebars",
+		"plugins" : [{
+			"name" : "Require Handlebars Plugin",
+			"src" : "https://raw.github.com/SlexAxton/require-handlebars-plugin/master/hbs.js"
+		}]
+	}, {
+		"name" : "JSON3",
+		"src" : "http://bestiejs.github.com/json3/lib/json3.js"
+	}, {
+		"name" : "Modernizr",
+		"src" : "http://modernizr.com/downloads/modernizr.js"
+	}, {
+		"name" : "Request Animation Frame",
+		"filename" : "request-animation-frame.js",
+		"src" : "https://raw.github.com/gist/1579671/7f515ade253afbc860dac1f84e21998d54359d79/rAF.js"
+	}, {
+		"name" : "Require.js",
+		"src" : "https://raw.github.com/jrburke/requirejs/master/require.js"
+	}, {
+		"name" : "Respond.js",
+		"filename" : "respond.js",
+		"src" : "https://raw.github.com/scottjehl/Respond/master/respond.src.js"
+	}]
+};
