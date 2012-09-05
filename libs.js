2a5cc44771cfe802a7a2bbe84a811e28d42d7f6a
Richard Herrera <rich@doctyper.com>
2012-09-04 18:03:42 -0700

    Updating libs.

--- libs.js
+++ libs.js
@@ -1,5 +1,8 @@
 module.exports = {
 	"libs" : [{
+		"name" : "jQuery",
+		"src" : "http://code.jquery.com/jquery.js"
+	}, {
 		"name" : "Handlebars",
 		"filename" : "handlebars.js",
 		"src" : "https://raw.github.com/SlexAxton/require-handlebars-plugin/master/Handlebars.js",
@@ -15,15 +18,7 @@ module.exports = {
 		"name" : "Modernizr",
 		"src" : "http://modernizr.com/downloads/modernizr.js"
 	}, {
-		"name" : "Request Animation Frame",
-		"filename" : "request-animation-frame.js",
-		"src" : "https://raw.github.com/gist/1579671/7f515ade253afbc860dac1f84e21998d54359d79/rAF.js"
-	}, {
 		"name" : "Require.js",
 		"src" : "https://raw.github.com/jrburke/requirejs/master/require.js"
-	}, {
-		"name" : "Respond.js",
-		"filename" : "respond.js",
-		"src" : "https://raw.github.com/scottjehl/Respond/master/respond.src.js"
 	}]
 };
