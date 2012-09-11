b0d9312dcaf6259b61c6fa57c4e7a94b620112d1
Richard Herrera <rich@doctyper.com>
2012-09-11 14:35:22 -0700

    Packaging initialize scripts to be imported by a require statement.

--- install.js
+++ install.js
@@ -2,56 +2,72 @@
 /*global jake, desc, task, error, pkg, installModule, parseFiles */
 "use strict";
 
-var fs = require("fs");
-var cp = require("child_process");
-var path = require("path");
-var installpath = path.join(__dirname, "project/static/js/libs/_install");
+module.exports = {
+	exec : function (exec, args, cwd, suppress, doneCB) {
+		var cp = require("child_process"),
+			child, data;
 
-var exec = function (exec, args, cwd, suppress, doneCB) {
-	process.stdin.resume();
+		child = cp.spawn(exec, args || [], {
+			cwd: cwd,
+			env: null,
+			setsid: true,
+			stdio: (suppress) ? null : "inherit"
+		});
 
-	var child = cp.spawn(exec, args || [], {
-		cwd: cwd,
-		env: null,
-		setsid: true
-	});
+		if (child.stdout) {
+			data = "";
 
-	process.stdin.resume();
-	process.stdin.pipe(child.stdin, {end: false});
+			child.stdout.on("data", function (buffer) {
+				data += buffer.toString();
+			});
+		}
 
-	if (!suppress) {
-		child.stdout.pipe(process.stdout);
-		child.stderr.pipe(process.stderr);
+		child.on("exit", function (code) {
+			doneCB(!code, data);
+		});
+	},
+
+	installExternalScripts : function () {
+		var fs = require("fs"),
+			path = require("path"),
+			installpath = path.join(__dirname, "project/static/js/libs/_install");
+
+		this.exec("node", [path.join(installpath, "installer")], null, false, function (success) {
+			if (!success) {
+				return this.exit("An error occurred while installing external libraries.");
+			}
+
+			if (fs.existsSync(installpath)) {
+				var installer = path.join(installpath, "installer.js");
+				var config = path.join(installpath, "libs.config.js");
+
+				if (fs.existsSync(installer)) {
+					fs.unlinkSync(installer);
+				}
+
+				if (fs.existsSync(config)) {
+					fs.unlinkSync(config);
+				}
+
+				fs.rmdirSync(installpath);
+			} else {
+				return this.exit("Can't find %s. Exiting.".replace("%s", installpath));
+			}
+
+			return this.exit();
+		}.bind(this));
+	},
+
+	exit : function (error) {
+		if (this.cb) {
+			this.cb(error);
+		} else {
+			process.exit();
+		}
+	},
+
+	run : function (cb) {
+		this.cb = cb;
+		this.installExternalScripts();
 	}
-
-	child.addListener("exit", function (code) {
-		doneCB(!code);
-	});
 };
-
-exec("node", [path.join(installpath, "installer")], null, false, function (success) {
-	if (!success) {
-		console.error("An error occurred while installing external libraries.");
-		process.exit(false);
-	}
-
-	if (fs.existsSync(installpath)) {
-		var installer = path.join(installpath, "installer.js");
-		var config = path.join(installpath, "libs.config.js");
-
-		if (fs.existsSync(installer)) {
-			fs.unlinkSync(installer);
-		}
-
-		if (fs.existsSync(config)) {
-			fs.unlinkSync(config);
-		}
-
-		fs.rmdirSync(installpath);
-	} else {
-		console.error("Can't find %s. Exiting.".replace("%s", installpath));
-		process.exit(false);
-	}
-
-	process.exit();
-});
